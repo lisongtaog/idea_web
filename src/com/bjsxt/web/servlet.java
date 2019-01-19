0655ed101f5cec7e9b5de48bcc0cf5e9c2944a14
single <1471444606@qq.com>
2019-01-19 18:34:27 +0800

    idea web 项目模板

---
+++ src/com/bjsxt/web/servlet.java
@@ -0,0 +1,26 @@
+package com.bjsxt.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "AddUser", urlPatterns = {"/addUser"})
+public class servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        String username = req.getParameter("username");
+        String userage = req.getParameter("userage");
+        System.out.println(username + " " + userage);
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: single
+  Date: 2019-01-18
+  Time: 16:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>北京尚学堂</title>
+  </head>
+  <body>
+  <form action="addUser" method="post">
+    用户名：<input type="text" name="username">
+    年龄：<input type="text" name="userage">
+    <input type="submit" value="OKOK">
+  </form>
+  </body>
+</html>
